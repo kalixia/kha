97521307c5635f732611b01ef3aa54ff1f09c194
Jerome Bernard <jerome.bernard@kalixia.com>
2012-12-16 00:47:27 +0100

    Tring to make build to work with both IntelliJ and Maven.

--- android/src/main/java/com/kalixia/ha/android/activities/DevicesActivity.java
+++ android/src/main/java/com/kalixia/ha/android/activities/DevicesActivity.java
@@ -2,7 +2,14 @@ package com.kalixia.ha.android.activities;
 
 import android.app.Activity;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.FragmentById;
+import com.googlecode.androidannotations.annotations.OptionsMenu;
+import com.kalixia.ha.android.R;
+import com.kalixia.ha.android.fragments.DevicesListFragment;
 
 @EActivity
+@OptionsMenu(R.menu.main)
 public class DevicesActivity extends Activity {
+    @FragmentById(R.id.devicesListFragment)
+    DevicesListFragment devicesListFragment;
 }
--- android/target/generated-sources/r/com/kalixia/ha/android/BuildConfig.java
+++
@@ -1,5 +0,0 @@
-package com.kalixia.ha.android;
-
-public final class BuildConfig {
-  public static final boolean DEBUG = true;
-}

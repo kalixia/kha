cdc949130eb3c28ddccd4d1ca863e136078d9245
Jerome Bernard <jerome.bernard@kalixia.com>
2013-05-28 15:27:37 +0200

    Package the gateway as an executable JAR instead of a WAR. Introduced GZIP support in Netty pipeline.

--- gateway/src/main/java/com/kalixia/ha/gateway/ApiServerChannelInitializer.java
+++ gateway/src/main/java/com/kalixia/ha/gateway/ApiServerChannelInitializer.java
@@ -11,6 +11,8 @@ import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpContentCompressor;
+import io.netty.handler.codec.http.HttpContentDecompressor;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpResponseEncoder;
@@ -42,8 +44,10 @@ public class ApiServerChannelInitializer extends ChannelInitializer<SocketChanne
         ChannelPipeline pipeline = ch.pipeline();
 
         pipeline.addLast("http-request-decoder", new HttpRequestDecoder());
+        pipeline.addLast("deflater", new HttpContentDecompressor());
         pipeline.addLast("http-object-aggregator", new HttpObjectAggregator(1048576));
         pipeline.addLast("http-response-encoder", new HttpResponseEncoder());
+        pipeline.addLast("inflater", new HttpContentCompressor());
 
         // Alters the pipeline depending on either REST or WebSockets requests
         pipeline.addLast("api-protocol-switcher", new ApiProtocolSwitcher(objectMapper));

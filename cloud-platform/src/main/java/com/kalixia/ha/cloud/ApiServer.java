f196bf6de4b2ba63bc6191dcd3fc7d0f25cfb777
Jerome Bernard <jerome.bernard@kalixia.com>
2013-06-17 16:33:10 +0200

    Updated to Netty 4.0.RC5 as Grapi did.

--- cloud-platform/src/main/java/com/kalixia/ha/cloud/ApiServer.java
+++ cloud-platform/src/main/java/com/kalixia/ha/cloud/ApiServer.java
@@ -3,6 +3,7 @@ package com.kalixia.ha.cloud;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.slf4j.Logger;
@@ -13,8 +14,6 @@ import java.util.concurrent.TimeUnit;
 public class ApiServer {
     private ServerBootstrap apiBootstrap;
     private final int port;
-    private NioEventLoopGroup bossGroup;
-    private NioEventLoopGroup workersGroup;
     private final ChannelHandler channelInitializer;
     private static final Logger LOGGER = LoggerFactory.getLogger(ApiServer.class);
 
@@ -25,11 +24,9 @@ public class ApiServer {
 
     public void start() {
         apiBootstrap = new ServerBootstrap();
-        bossGroup = new NioEventLoopGroup();
-        workersGroup = new NioEventLoopGroup();
         try {
             // the cloudPlatform will only have a few connections, so OIO is likely to be faster than NIO in this case!
-            apiBootstrap.group(bossGroup, workersGroup)
+            apiBootstrap.group(new NioEventLoopGroup(), new NioEventLoopGroup())
                     .channel(NioServerSocketChannel.class)
                     .localAddress(port)
                     .childOption(ChannelOption.SO_KEEPALIVE, true)
@@ -45,6 +42,9 @@ public class ApiServer {
     }
 
     public void stop() throws InterruptedException {
+        EventLoopGroup bossGroup = apiBootstrap.group();
+        EventLoopGroup workersGroup = apiBootstrap.childGroup();
+
         bossGroup.shutdownGracefully();
         workersGroup.shutdownGracefully();
         bossGroup.awaitTermination(1, TimeUnit.MINUTES);

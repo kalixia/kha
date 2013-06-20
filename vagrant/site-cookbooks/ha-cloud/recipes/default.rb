9406b57d8c5047b3c11c7160cff02c90907821b5
Jerome Bernard <jerome.bernard@kalixia.com>
2013-06-20 12:19:59 +0200

    Vagrant: WIP on Nginx serving HTML pages.

--- vagrant/site-cookbooks/ha-cloud/recipes/default.rb
+++ vagrant/site-cookbooks/ha-cloud/recipes/default.rb
@@ -10,6 +10,16 @@
 # Create virtual host for Nginx
 include_recipe 'nginx'
 
+# Deploy static resources to be served via Nginx
+#directory "/var/www" do
+#	owner "root"
+#	group "root"
+#end
+#remote_directory "/var/www/gateway" do
+#	files_owner node['nginx']['user']
+#	source "gateway"
+#end	
+
 template "#{node['nginx']['dir']}/sites-available/ha-cloud" do
   source "ha-cloud.erb"
   owner "root"

0d492cb832d91432ea8608343ff2c1c134a55aa6
Jerome Bernard <jerome.bernard@kalixia.com>
2013-07-31 19:57:16 +0200

    Added some common sensors and quantities.

--- model/src/main/java/com/kalixia/ha/model/sensors/Sensor.java
+++ model/src/main/java/com/kalixia/ha/model/sensors/Sensor.java
@@ -1,8 +1,9 @@
 package com.kalixia.ha.model.sensors;
 
+import javax.measure.quantity.Quantity;
 import javax.measure.unit.Unit;
 
-public interface Sensor {
+public interface Sensor<Q extends Quantity> {
     String getName();
-    Unit getUnit();
+    Unit<Q> getUnit();
 }

851f5fc235134fba73a416f5700c24fa9795c840
Jerome Bernard <jerome.bernard@kalixia.com>
2013-08-03 15:24:53 +0200

    Small addition to sensor configuration.

--- model/src/main/java/com/kalixia/ha/model/configuration/SensorConfiguration.java
+++ model/src/main/java/com/kalixia/ha/model/configuration/SensorConfiguration.java
@@ -6,8 +6,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * Basic configuration for sensor.
  */
 public class SensorConfiguration extends Configuration {
+    @JsonProperty("name")
+    private String name;
+
     @JsonProperty("enabled")
     private boolean enabled;
+
+    public String getName() {
+        return name;
+    }
 
     public boolean isEnabled() {
         return enabled;

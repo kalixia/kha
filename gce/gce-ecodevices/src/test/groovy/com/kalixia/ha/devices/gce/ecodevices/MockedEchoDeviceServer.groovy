b72a9264b805e89f29d14943d9c85c748247e408
Jerome Bernard <jerome.bernard@kalixia.com>
2014-05-05 10:46:00 +0200

    EcoDevice testing: replaced Vert.x with RxNetty.

--- gce/gce-ecodevices/src/test/groovy/com/kalixia/ha/devices/gce/ecodevices/MockedEchoDeviceServer.groovy
+++ gce/gce-ecodevices/src/test/groovy/com/kalixia/ha/devices/gce/ecodevices/MockedEchoDeviceServer.groovy
@@ -1,48 +1,46 @@
 package com.kalixia.ha.devices.gce.ecodevices
 
 import groovy.util.logging.Slf4j
-import org.vertx.groovy.core.Vertx
-import org.vertx.groovy.core.http.HttpServerRequest
-import org.vertx.groovy.core.http.HttpServer
+import io.netty.buffer.ByteBuf
+import io.netty.handler.codec.http.HttpResponseStatus
+import io.reactivex.netty.RxNetty
+import io.reactivex.netty.protocol.http.server.HttpServer
+import io.reactivex.netty.protocol.http.server.HttpServerRequest
+import io.reactivex.netty.protocol.http.server.HttpServerResponse
+import io.reactivex.netty.protocol.http.server.RequestHandler
 
 @Slf4j("LOGGER")
 class MockedEchoDeviceServer {
-    def HttpServer server
+    def HttpServer<ByteBuf, ByteBuf> server;
 
     def start() {
-        Vertx vertx = Vertx.newVertx("localhost")
-        server = vertx.createHttpServer()
-        LOGGER.info("Starting VertX server...")
-
-        server.requestHandler { HttpServerRequest req ->
-            String requestURI = req.absoluteURI.toString()
-            def resourceURI = requestURI.substring(requestURI.lastIndexOf('/') + 1, requestURI.length())
-            LOGGER.info "Should load resource $resourceURI"
-            def resource = getClass().getResource(resourceURI)
-            if (resource == null) {
-                LOGGER.error "Oops. Resource is null"
-                req.response.with {
-                    statusCode = 404
-                    end()
+        LOGGER.info("Starting fake EcoDevice HTTP server...")
+        server = RxNetty.createHttpServer(12345, new RequestHandler<ByteBuf, ByteBuf>() {
+            @Override
+            rx.Observable<Void> handle(HttpServerRequest<ByteBuf> request, HttpServerResponse<ByteBuf> response) {
+                String requestURI = request.uri
+                def resourceURI = requestURI.substring(requestURI.lastIndexOf('/') + 1, requestURI.length())
+                LOGGER.info "Should load resource $resourceURI"
+                def resource = getClass().getResource(resourceURI)
+                if (resource == null) {
+                    LOGGER.error "Oops. Resource is null"
+                    response.status = HttpResponseStatus.NOT_FOUND
+                    return response.writeStringAndFlush("Not Found\n")
+                } else {
+                    response.status = HttpResponseStatus.OK
+                    response.headers.add("Content-Type", "application/xml")
+                    response.writeStringAndFlush(resource.text)
+                    LOGGER.info "Sent proper data for $resourceURI"
                 }
-            } else {
-                req.response.with {
-                    putHeader "Content-Type", "application/xml"
-                    end(resource.text)
-                }
-                LOGGER.info "Sent proper data for $resourceURI"
             }
-        }
+        });
 
-        server.listen(12345, 'localhost') { asyncResult -> println "Listen succeeded ? ${asyncResult.succeeded}" }
+        server.start();
     }
 
     def stop() {
-        LOGGER.info("Stopping VertX server...")
-        server.close()
+        LOGGER.info("Stopping fake EcoDevice HTTP server...")
+        server.shutdown()
     }
 
-    public static void main(String[] args) {
-        new MockedEchoDeviceServer().run()
-    }
 }

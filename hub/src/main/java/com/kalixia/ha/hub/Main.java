1d7b7054c6704b108d4feccff709b71cf995ef07
Jerome Bernard <jerome.bernard@kalixia.com>
2014-05-17 11:33:38 +0200

    Hub: logging improvements.

--- hub/src/main/java/com/kalixia/ha/hub/Main.java
+++ hub/src/main/java/com/kalixia/ha/hub/Main.java
@@ -1,6 +1,8 @@
 package com.kalixia.ha.hub;
 
 import dagger.ObjectGraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 
@@ -8,11 +10,15 @@ public class Main {
     @Inject
     Hub hub;
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public void start() throws InterruptedException {
         hub.start();
     }
 
     public static void main(String[] args) throws InterruptedException {
+        logger.info("Starting Kha Hub...");
+
         ObjectGraph objectGraph = ObjectGraph.create(new HubModule());
         Main main = objectGraph.get(Main.class);
         main.start();
